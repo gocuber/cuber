b13dd22e8972a6c9bd6c5739146267f66f661f6d
gocuber <dafei.net@gmail.com>
2018-06-28 14:23:16 +0800

    database config

--- config/app.php
+++ config/app.php
@@ -6,20 +6,21 @@
  * @author Cuber <dafei.net@gmail.com>
  */
 
-$_G = [];
+return [
 
-// 数据库配置
+    // 数据库配置
 
-$_G['db']['default'] = [
-    'host'     => '127.0.0.1',
-    'port'     => 3306,
-	'username' => 'root',
-	'password' => '',
-	'database' => 'db_cube',
-	'charset'  => 'utf8',
-	'driver'   => 'mysql',
-    'slave'    => [
-        ['host' => 'slave1', 'port' => 3306],  // slave1
-        ['host' => 'slave2', 'port' => 3306],  // slave2 slave3 slave4 ...
+    'db' => [
+        'default' => [
+            'host'     => env('DB_HOST', '127.0.0.1'),
+            'port'     => env('DB_PORT', '3306'),
+        	'username' => env('DB_USERNAME', 'root'),
+        	'password' => env('DB_PASSWORD', '123456'),
+        	'database' => env('DB_DATABASE', 'db_cuber'),
+        	'charset'  => 'utf8mb4',
+        	'driver'   => 'mysql',
+            'slave'    => [], // slave1 slave2 slave3 slave4 ...
+        ],
     ],
+
 ];

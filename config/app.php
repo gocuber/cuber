49112d3da4106833e845af71323921b3cca860a7
cuber <cuber@gocuber.com>
2018-10-16 17:41:19 +0800

    config

--- config/app.php
+++ config/app.php
@@ -6,6 +6,15 @@
  * @author Cuber <dafei.net@gmail.com>
  */
 
+$db_default_slave = [];
+if (!empty(env('DB_DEFAULT_SLAVE'))) {
+    $slave = explode(',', env('DB_DEFAULT_SLAVE', ''));
+    foreach ($slave as $value) {
+        $v = explode(':', $value);
+        $db_default_slave[] = ['host'=>$v[0], 'port'=>isset($v[1])?$v[1]:3306];
+    }
+}
+
 return [
 
     'name' => 'Cuber',
@@ -14,30 +23,83 @@ return [
 
     'charset' => 'utf-8',
 
-    'app_debug' => true,
+    'debug' => env('DEBUG', false),
+
+    // controllers namespace prefix
+    'controllers_namespace' => 'App\\Controllers\\',
+
+    // models namespace prefix
+    'models_namespace' => 'App\\Models\\',
+
+    // views dir
+    'views' => BASE_PATH . 'app/views/',
 
     // alias
-
     'alias' => [
         'Route' => 'Cuber\\Foundation\\Route',
         'View' => 'Cuber\\Foundation\\View',
     ],
 
-    // 数据库配置
+    // cookie配置
+    'cookie_prefix' => null,  // cookie 前缀
+    'cookie_domain' => null,  // cookie 域
 
+    // Mysql数据库配置
     'db' => [
         'default' => [
-            'host'     => '127.0.0.1',
-            'port'     => '3306',
-            'username' => 'root',
-            'password' => '123456',
-            'database' => 'db_cuber',
+            'host'     => env('DB_DEFAULT_HOST', '127.0.0.1'),
+            'port'     => env('DB_DEFAULT_PORT', 3306),
+            'username' => env('DB_DEFAULT_USERNAME', ''),
+            'password' => env('DB_DEFAULT_PASSWORD', ''),
+            'database' => env('DB_DEFAULT_DATABASE', ''),
             'charset'  => 'utf8mb4',
             'driver'   => 'mysql',
-            'slave'    => [], // slave1 slave2 slave3 slave4 ...
+            'slave'    => $db_default_slave,
         ],
     ],
 
+    // Memcache配置
+    'memcache' => [
+        'default' => [
+            'host' => env('MEM_DEFAULT_HOST', '127.0.0.1'),
+            'port' => env('MEM_DEFAULT_PORT', 11211),
+        ],
+    ],
+
+    // Redis配置
+    'is_redis' => env('IS_REDIS', false),
+    'redis' => [
+        'default' => [
+            'host'  => env('REDIS_DEFAULT_HOST', '127.0.0.1'),
+            'port'  => env('REDIS_DEFAULT_PORT', 6379),
+            'auth'  => env('REDIS_DEFAULT_AUTH', ''),
+            'slave' => [],
+        ],
+    ],
+
+    // FileCache配置
+    'filecache' => [
+        'default' => [
+            'default'   => BASE_PATH . 'storage/filecache/default/',  // 缓存目录
+            'is_subdir' => 1,                                         // 是否自动生成子级缓存目录 默认1是 0否
+        ],
+        'session' => [
+            'dir'       => BASE_PATH . 'storage/filecache/session/',
+            'is_subdir' => 0,
+        ],
+    ],
+
+    // 异常
     'error_log' => BASE_PATH . 'storage/logs/',
+
+    // authcode
+    'authcode' => env('AUTH_CODE', ''),
+
+    // rsa
+    'rsa' => [
+        'dir'  => BASE_PATH . 'storage/rsa/',
+        'num'  => 200,
+        'code' => env('RSA_CODE', ''),
+    ],
 
 ];
